093ae4ee80a82a6d4b2b8469c6e1034654b8d900
cheng-shiwen <chengshiwen0103@gmail.com>
2014-09-11 00:36:58 +0800

    Update Baidu-Push-Server-SDK-Python

---
+++ README.md
@@ -0,0 +1,45 @@
+百度云推送服务端SDK Python版本
+==============
+
+将百度云推送（Push）服务端的所有 API 封装成一个类 `Channel.py`，通过对该类的简单初始化，即可调用其内部的各种方法，使用百度云推送服务。
+
+`Channel.py` 提供的方法和服务端 API 对应，是对服务端 REST API 的封装，详细的 REST API 请参考[官网API列表](http://developer.baidu.com/wiki/index.php?title=docs/cplat/push/api/list)。
+
+
+### 代码构成
+
+*   `Channel.py` -- SDK 脚本，包含对外提供的所有接口
+*   `sample/sample.py` -- 使用 SDK 的 Demo 文件
+
+
+### 依赖库
+
+*   [requests](http://python-requests.org)
+
+
+### 一般规则
+
+*   所有函数的参数和返回值中如果有中文，必须是UTF-8编码
+*   不需要对函数参数进行urlencode
+*   错误信息见 [错误码定义](http://developer.baidu.com/wiki/index.php?title=docs/cplat/push/api#JSON.E5.93.8D.E5.BA.94.E7.BC.96.E7.A0.81)
+
+
+### 版本更迭
+
+该 Python SDK 主要由 [luvchh](https://github.com/Argger) 完成了大部分的工作
+
+**第一版：**
+
+由[luvchh](https://github.com/Argger) 提供，并开放在 Github 上：<https://github.com/Argger/pusher_python_sdk>
+
+**第二版：**
+
+由[blacklaw0](https://github.com/blacklaw0) 修改，Github 地址：<https://github.com/blacklaw0/pusher_python_sdk>
+
+**第三版：**
+
+由 [gfreezy](https://github.com/gfreezy) 修改，Github 地址：<https://github.com/gfreezy/pusher_python_sdk>
+
+**第四版：**
+
+由 [cheng-shiwen](https://github.com/cheng-shiwen) 更新，Github 地址：<https://github.com/cheng-shiwen/Baidu-Push-Server-SDK-Python>
---
+++ sample/sample.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+# coding=utf-8
+
+import sys
+import time
+sys.path.append("..")
+from Channel import Channel
+
+# 以下只是测试数据，请使用者自行修改为可用数据
+apiKey = "76Yi0ZBGGV2HrAziIiYEFtRh"
+secretKey = "xxxxxxxxxxxxx"
+user_id = "1105115563847474869"
+channel_id = 3944730196422489622
+
+message = "{'title':'baidu push','description':'message from python sdk'}"
+message_key = "key1"
+tagname = "test_tag"
+
+def test_pushMessage_to_user():
+    c = Channel(apiKey, secretKey)
+    push_type = Channel.PUSH_TO_USER
+    optional = dict()
+    optional[Channel.USER_ID] = user_id
+    optional[Channel.CHANNEL_ID] = channel_id
+    # 推送通知类型
+    optional[Channel.MESSAGE_TYPE] = Channel.PUSH_NOTIFICATION
+    ret = c.pushMessage(push_type, message, message_key, optional)
+    print ret
+
+def test_pushMessage_to_tag():
+    c = Channel(apiKey, secretKey)
+    push_type = Channel.PUSH_TO_TAG
+    tag_name = 'push'
+    optional = dict()
+    optional[Channel.TAG_NAME] = tag_name
+    ret = c.pushMessage(push_type, message, message_key, optional)
+    print ret
+
+def test_pushMessage_to_all():
+    c = Channel(apiKey, secretKey)
+    push_type = Channel.PUSH_TO_ALL
+    optional = dict()
+    ret = c.pushMessage(push_type, message, message_key, optional)
+    print ret
+
+
+def test_queryBindList():
+    c = Channel(apiKey, secretKey)
+    optional = dict()
+    optional[Channel.CHANNEL_ID] = channel_id
+    ret = c.queryBindList(user_id, optional)
+    print ret
+
+def test_verifyBind():
+    c = Channel(apiKey, secretKey)
+    optional = dict()
+    optional[Channel.DEVICE_TYPE] = Channel.DEVICE_ANDRIOD;
+    ret = c.verifyBind(user_id, optional)
+    print ret
+
+def test_fetchMessage():
+    c = Channel(apiKey, secretKey)
+    ret = c.fetchMessage(user_id)
+    print ret
+
+def test_deleteMessage():
+    c = Channel(apiKey, secretKey)
+    msg_id = "111"
+    ret = c.deleteMessage(user_id, msg_id)
+    print ret
+
+def test_setTag():
+    c = Channel(apiKey, secretKey)
+    optional = dict()
+    optional[Channel.USER_ID] = user_id
+    ret = c.setTag(tagname, optional)
+    print ret
+
+def test_fetchTag():
+    c = Channel(apiKey, secretKey)
+    ret = c.fetchTag()
+    print ret
+
+def test_deleteTag():
+    c = Channel(apiKey, secretKey)
+    optional = dict()
+    optional[Channel.USER_ID] = user_id
+    ret = c.deleteTag(tagname, optional)
+    print ret
+
+def test_queryUserTag():
+    c = Channel(apiKey, secretKey)
+    ret = c.queryUserTag(user_id)
+    print ret
+
+def test_queryDeviceType():
+    c = Channel(apiKey, secretKey)
+    ret = c.queryDeviceType(channel_id)
+    print ret
+
+test_pushMessage_to_user()
+
+"""
+if __name__ == '__main__':
+    test_pushMessage_to_user()
+    time.sleep(1)
+    test_pushMessage_to_tag()
+    time.sleep(1)
+    test_pushMessage_to_all()
+    time.sleep(1)
+    test_queryBindList()
+    time.sleep(1)
+    test_verifyBind()
+    time.sleep(1)
+    test_fetchMessage()
+    time.sleep(1)
+    test_deleteMessage()
+    time.sleep(1)
+    test_setTag()
+    time.sleep(1)
+    test_fetchTag()
+    time.sleep(1)
+    test_deleteTag()
+    time.sleep(1)
+    test_queryUserTag()
+    time.sleep(1)
+    test_queryDeviceType()
+    time.sleep(1)
+"""
